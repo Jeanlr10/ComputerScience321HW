f8b1f2ce573c0198f010125a0ed49d676494ac7e
JeanLuc Robitaille <jlr@robitaille.us>
2024-09-04 09:37:48 -0400

    Before Class 9/4

--- Class_Method_Lab/ClassMethodLab.java
+++ Class_Method_Lab/ClassMethodLab.java
@@ -4,9 +4,11 @@ public class ClassMethodLab{
     public static void main(String[] args)
     {
         String str = "Hello"; // sets str to a string with both capital and lowercase characters
-        System.out.println(str);
-        System.out.println(CaseChange.allUpper(str)); // tests the allUpper method converting str to all uppercase
-        System.out.println(CaseChange.allLower(str)); // tests the allLower method converting str to all lowercase
+        System.out.println("     original: "+str);
+        System.out.println("all uppercase: "+CaseChange.allUpper(str)); // tests the allUpper method converting str to all uppercase
+        System.out.println("all lowercase: "+CaseChange.allLower(str)); // tests the allLower method converting str to all lowercase
+        //
+        //
         double milli_to_centi=PrefixConvert.prefix_convert("milli",50,"centi"); // tests small prefixes to large prefixes
         double kilo_to_milli = PrefixConvert.prefix_convert("kilo",3,"MILLI"); // tests large prefixes to small prefixes as well as input standardization
         System.out.println("50 millimeters is "+milli_to_centi+" centimeters");
